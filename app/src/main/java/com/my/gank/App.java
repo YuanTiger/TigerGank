7993ad6598748f651f06d2a890e07e578af3e42c
孟远 <mengy@ruwe.cn>
2017-08-09 14:28:21 +0800

    init

---
+++ app/src/main/java/com/my/gank/App.java
@@ -0,0 +1,23 @@
+package com.my.gank;
+
+import android.app.Application;
+import android.content.Context;
+
+/**
+ * Author：mengyuan
+ * Date  : 2017/8/4下午5:50
+ * E-Mail:mengyuanzz@126.com
+ * Desc  :
+ */
+
+public class App extends Application {
+
+    public static Context context;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        context = this.getApplicationContext();
+    }
+}
---
+++ app/src/main/java/com/my/gank/SplashActivity.java
@@ -0,0 +1,11 @@
+package com.my.gank;
+
+/**
+ * Author：mengyuan
+ * Date  : 2017/8/4下午5:51
+ * E-Mail:mengyuanzz@126.com
+ * Desc  :
+ */
+
+public class SplashActivity extends BaseActivity {
+}
